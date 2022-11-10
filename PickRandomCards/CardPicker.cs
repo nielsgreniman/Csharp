66e70dc4328879bce582fcea64dcfbfe0ef62202
nielsgreniman <105637299+nielsgreniman@users.noreply.github.com>
2022-11-10 22:26:59 +0100

    added PickRandomCards

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace PickRandomCards
+{
+	class CardPicker
+	{
+		public static string[] PickSomeCards(int numberOfCards)
+		{
+			string[] pickedCards = new string[numberOfCards];
+			for (int i = 0; i < numberOfCards; i++)
+			{
+				pickedCards[i] = RandomValue() + " of " + RandomSuit();
+			}
+
+			return pickedCards;
+		}
+
+		private static string RandomValue()
+		{
+			throw new NotImplementedException();
+		}
+
+		private static string RandomSuit()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
+
---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            PickRandomCards();
+        }
+    }
+}
