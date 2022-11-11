a95386a39dc87d8ce58c126c52814a0696a56e06
nielsgreniman <105637299+nielsgreniman@users.noreply.github.com>
2022-11-11 13:27:15 +0100

    CardPicker app added

---
+++ PickACardBlazor/CardPicker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PickACardBlazor
+{
+	class CardPicker
+	{
+		static Random random = new Random();
+
+		public static string[] PickSomeCards(int numberOfCards)
+		{
+			string[] pickedCards = new string[numberOfCards];
+			for (int i = 0; i < numberOfCards; i++)
+			{
+				pickedCards[i] = RandomValue() + " of " + RandomSuit();
+			}
+
+			return pickedCards;
+		}
+
+		private static string RandomValue()
+		{
+			int value = random.Next(1, 14);
+			if (value == 1) return "Ace";
+			if (value == 11) return "Jack";
+			if (value == 12) return "Queen";
+            if (value == 13) return "King";
+			return value.ToString();
+        }
+
+		private static string RandomSuit()
+		{
+			// get random number from 1 to 4
+			int value = random.Next(1, 5);
+
+			// return a suit
+			if (value == 1) return "Spades";
+			if (value == 2) return "Hearts";
+			if (value == 3) return "Clubs";
+			return "Diamonds";
+		}
+	}
+}
+
